6b8676366a1a8ae741d1c0aff779a2992cbbbc3b
Foxvore <imjohani2005@gmail.com>
2023-01-16 15:07:22 +0700

    Fix Delete Action

--- app/Http/Controllers/GuruController.php
+++ app/Http/Controllers/GuruController.php
@@ -100,7 +100,7 @@ class GuruController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy(Guru $guru)
     {
         $mengajar = Mengajar::where('guru_id', $guru->id)->first();
 
