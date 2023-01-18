136a47dcbf29d56d8d7d5aac74edfa475d9996e2
Foxvore <imjohani2005@gmail.com>
2023-01-18 13:17:24 +0700

    Add Mengajar Routes

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\JurusanController;
 use App\Http\Controllers\MapelController;
 use App\Http\Controllers\KelasController;
 use App\Http\Controllers\SiswaController;
+use App\Http\Controllers\MengajarController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
@@ -91,6 +92,15 @@ Route::prefix('siswa')->group(function(){
     Route::get('/edit/{siswa}', [SiswaController::class, 'edit']);
     Route::post('/update/{siswa}', [SiswaController::class, 'update']);
     Route::get('/destroy/{siswa}', [SiswaController::class, 'destroy']);
+});
+
+Route::prefix('mengajar')->group(function(){
+    Route::get('/index', [MengajarController::class, 'index']);
+    Route::get('/create', [MengajarController::class, 'create']);
+    Route::post('/store', [MengajarController::class, 'store']);
+    Route::get('/edit/{mengajar}', [MengajarController::class, 'edit']);
+    Route::post('/update/{mengajar}', [MengajarController::class, 'update']);
+    Route::get('/destroy/{mengajar}', [MengajarController::class, 'destroy']);
 });
 
 require __DIR__.'/auth.php';
